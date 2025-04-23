4b2ea3b6470ed408d440439726379e777f46db36
WOOJOOHAHM <hahmwj@Home>
2025-04-23 21:39:31 +0900

    first commit

--- version1/app.py
+++ version1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def about():
-    return render_template('about.html')
+    return render_template('template.html')
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
